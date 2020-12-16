c4e7a3c78d23912a42014fed7d5af5b1834009f5
Ekenayy <ekenayy@gmail.com>
2020-12-16 11:02:12 -0500

    commit tree 4fcbea582dd8d04d19364e101432f94af61aae42

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+let formData = {
+    userName: 'Ekene',
+    userEmail: '123@gmail.com'
+}
+
+let h1 = document.createElement('h1') 
+ document.body.append(h1)
+
+
+function submitData (object) {
+    
+    let configObj = {
+        method: 'Post',
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(object)
+      };
+
+    fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+         return response.json();
+  })
+  .then(function(object) {
+      h1.innerText = object.id
+    
+  })
+  .catch(function(error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  }); 
+
+
+}
